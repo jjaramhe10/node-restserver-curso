23f1c07bd83190f95561c1a80ff9fae81b6ba4e3
Camilo <mac@Mac-mini-de-Mac.local>
2019-07-27 21:33:16 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,27 @@
-// ==========================
-//Puerto
-//===========================
-
+// ============================
+//  Puerto
+// ============================
 process.env.PORT = process.env.PORT || 3000;
+
+
+
+// ============================
+//  Entorno
+// ============================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ============================
+//  Base de datos
+// ============================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+
+} else {
+    urlDB = 'mongodb+srv://jjaramhe:camilo10@cluster0-i7ibm.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 require('./config/config');
 
 const express = require('express');
+
+const mongoose = require('mongoose');
+
+
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -13,49 +18,16 @@ app.use(bodyParser.json())
 
 
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
-});
+app.use(require('./routes/usuario'));
+
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
 
 
-
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-
-    } else {
-
-        res.json({
-            persona: body
-        });
-    }
-});
-
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-
-        id
-    });
-});
-
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
 });
 
 
 app.listen(process.env.PORT, () => {
-
     console.log('Escuchando puerto: ', process.env.PORT);
 });
