6650306eafe617bf78a889c6810f0642935e7d6d
Camilo <mac@Mac-mini-de-Mac.local>
2019-07-27 22:01:52 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 
 } else {
-    urlDB = 'mongodb+srv://jjaramhe:camilo10@cluster0-i7ibm.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
