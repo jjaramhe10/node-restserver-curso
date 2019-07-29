b61accca0f28e9a37106eb805ff950f32cdde281
Camilo <mac@Mac-mini-de-Mac.local>
2019-07-29 10:20:13 -0500

    Login normal seccion 10

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
 
+const bcrypt = require('bcrypt');
 const _ = require('underscore');
 
-
 const Usuario = require('../models/usuario');
-
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion');
 
 const app = express();
 
 
-app.get('/usuario', function(req, res) {
+app.get('/usuario', verificaToken, (req, res) => {
+
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -18,11 +18,11 @@ app.get('/usuario', function(req, res) {
     let limite = req.query.limite || 5;
     limite = Number(limite);
 
-
     Usuario.find({ estado: true }, 'nombre email role estado google img')
         .skip(desde)
         .limit(limite)
         .exec((err, usuarios) => {
+
             if (err) {
                 return res.status(400).json({
                     ok: false,
@@ -37,18 +37,18 @@ app.get('/usuario', function(req, res) {
                     usuarios,
                     cuantos: conteo
                 });
+
             });
 
 
         });
 
+
 });
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let body = req.body;
-
-
 
     let usuario = new Usuario({
         nombre: body.nombre,
@@ -57,7 +57,9 @@ app.post('/usuario', function(req, res) {
         role: body.role
     });
 
+
     usuario.save((err, usuarioDB) => {
+
         if (err) {
             return res.status(400).json({
                 ok: false,
@@ -65,30 +67,21 @@ app.post('/usuario', function(req, res) {
             });
         }
 
-        // usuarioDB.password = null;
-
         res.json({
             ok: true,
             usuario: usuarioDB
-
         });
+
+
     });
 
 
 });
 
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let id = req.params.id;
-    let body = _.pick(req.body, );
-
-    ['nombre',
-        'email',
-        'img',
-        'role',
-        'estado'
-    ];
-
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
 
     Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
 
@@ -100,28 +93,28 @@ app.put('/usuario/:id', function(req, res) {
         }
 
 
+
         res.json({
             ok: true,
             usuario: usuarioDB
-
         });
 
     })
 
 });
 
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
 
 
     let id = req.params.id;
-    //  Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+
+    // Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
 
     let cambiaEstado = {
         estado: false
     };
+
     Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
-
-
 
         if (err) {
             return res.status(400).json({
@@ -131,24 +124,25 @@ app.delete('/usuario/:id', function(req, res) {
         };
 
         if (!usuarioBorrado) {
-
             return res.status(400).json({
                 ok: false,
                 err: {
                     message: 'Usuario no encontrado'
                 }
             });
-
         }
 
         res.json({
             ok: true,
             usuario: usuarioBorrado
         });
+
     });
 
 
+
 });
+
 
 
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,7 @@
 require('./config/config');
 
 const express = require('express');
-
 const mongoose = require('mongoose');
-
 
 
 const app = express();
@@ -17,15 +15,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
+// Configuración global de rutas
+app.use(require('./routes/index'));
 
-app.use(require('./routes/usuario'));
 
-mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
 
+mongoose.connect(process.env.URLDB, (err, res) => {
 
     if (err) throw err;
+
     console.log('Base de datos ONLINE');
+
 });
+
 
 
 app.listen(process.env.PORT, () => {
